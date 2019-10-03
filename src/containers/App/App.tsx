2acf50c3c8549535a958f8f3919d13db6fc8585a
Balazs Bereczky <bereczkybalazs1@gmail.com>
2019-10-04 00:02:23 +0200

    refactor

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Weather from '../Weather/Weather'
 
 const App: React.FC = () => {
     return (
         <div className="app">
-            <h1 className="app__title">Hello World</h1>
+            <Weather />
         </div>
     );
 }
