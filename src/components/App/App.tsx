161700be028e657a0a5cdfc026a9b67efb6d07ec
Balazs Bereczky <bereczkybalazs1@gmail.com>
2019-10-03 22:58:19 +0200

    refactor to components with scss

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <h1 className="app__title">Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
