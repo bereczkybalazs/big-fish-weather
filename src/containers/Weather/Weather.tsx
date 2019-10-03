fb6f1fc88ed5ea3dd5296d727718c92df58a13c4
Balazs Bereczky <bereczkybalazs1@gmail.com>
2019-10-04 00:02:37 +0200

    create weather component

---
+++ src/containers/Weather/Weather.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Weather extends React.Component {
+
+
+    render(): React.ReactElement<any>{
+        return (
+            <div>
+                Weather
+            </div>
+        );
+    }
+}
+
+export default Weather;
